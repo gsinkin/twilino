0ca04238437c834de1ec5bbff858a4cb3819e836
Gabriel Sinkin <gabriel.sinkin@gmail.com>
2013-03-14 14:06:19 -0700

    Added app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twilino::Application.config.secret_token = 'c596519a64a3b689bd2371eb1e9bfa9731efd4b46c35646a5ea37ec96af2af681aeb1165dde20a7061e14e8577b896153c9a6eb6d210b49f7fd7435997bc8ecc'
